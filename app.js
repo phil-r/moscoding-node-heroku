241415afb62eb4fb82bfc406a517773227e74856
Phil <phil.ficus@gmail.com>
2015-07-12 02:01:06 +0300

    port fix

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = process.env.PORT || 8080;
+var ip = 'localhost';
+
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
+});
+
+app.use(express.static(__dirname+'/public'));
+
+http.listen(port, function () {
+  console.log('Our app at http://' + ip + ':' + port);
+});
