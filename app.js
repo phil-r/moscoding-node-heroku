fff549da8dbc03b140811a9bf1c8071dbc20c0b4
Phil <phil.ficus@gmail.com>
2015-11-01 16:19:31 +0300

    Added channels support

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 var ip = 'localhost';
 
+var messages = []; // 1
+var channels = [
+  {name: "Public", path:'/'},
+  {name: "Random", path:'/random'},
+  {name: "MosCoding", path:'/mcs'}
+];
+
 io.on('connection', function(socket){
-  socket.on('message', function(msg){
+  socket.emit('messages', messages); // 3
+  socket.emit('channels', channels);
+  socket.on('message', function(msg){ // сюда приходят сообщения
+    messages.push(msg); // 2
     io.emit('message', msg);
   });
 });
@@ -15,8 +25,14 @@ io.on('connection', function(socket){
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(req, res){
-    res.redirect('/chat.html');
+  res.sendFile(__dirname+'/public/chat.html');
 });
+
+app.get('/:id', function(req, res){
+  res.sendFile(__dirname+'/public/chat.html');
+});
+
+
 
 http.listen(port, function () {
   console.log('Our app at http://' + ip + ':' + port);
